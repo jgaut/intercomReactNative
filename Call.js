be253ac3a3552646848e01a5cd65517dc32aa207
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-01-21 12:11:24 +0100

    Update Call.js

--- Call.js
+++ Call.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, WebView } from 'react-native';
+import { View, Linking } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import { createAppContainer } from 'react-navigation';
 
